e3c72217115f3e3decaceee2587d875ee0b1eb3a
jclagoria <lagoria79@gmail.com>
2021-05-05 20:59:45 -0300

    add first version of proyect

---
+++ backend-service/top-popular-movies-series/src/main/java/com/lagoria/imdbservice/TopPopularMoviesSeriesApplication.java
@@ -0,0 +1,13 @@
+package com.lagoria.imdbservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopPopularMoviesSeriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopPopularMoviesSeriesApplication.class, args);
+	}
+
+}
