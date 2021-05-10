52a547e3722c3fb79b0f53a29d5e9861b1778b0a
jclagoria <lagoria79@gmail.com>
2021-05-10 00:34:54 -0300

    add new service

---
+++ backend-service/movie-section/imdb-youtube-service/src/main/java/com/lagoria/imdbservice/youtube/ImdbYoutubeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lagoria.imdbservice.youtube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImdbYoutubeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImdbYoutubeServiceApplication.class, args);
+	}
+
+}
---
+++ backend-service/movie-section/imdb-youtube-service/src/test/java/com/lagoria/imdbservice/youtube/ImdbYoutubeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lagoria.imdbservice.youtube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImdbYoutubeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
